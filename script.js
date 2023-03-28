2d38b7130e34a4a9377b81eb9b7c5fde35421b8e
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-03-27 17:34:41 -0700

    day 6(more css)

--- script.js
+++ script.js
@@ -2,100 +2,118 @@
 let fullScreenBtn = document.getElementById("fullscreen-btn");
 
 fullScreenBtn.addEventListener("click", function () {
- let docElm = document.documentElement;
- if (docElm.requestFullscreen) {
-  docElm.requestFullscreen();
- } else if (docElm.webkitRequestFullScreen) {
-  docElm.webkitRequestFullScreen();
- } else if (docElm.mozRequestFullScreen) {
-  docElm.mozRequestFullScreen();
- } else if (docElm.msRequestFullscreen) {
-  docElm.msRequestFullscreen();
- }
+  let docElm = document.documentElement;
+  if (docElm.requestFullscreen) {
+    docElm.requestFullscreen();
+  } else if (docElm.webkitRequestFullScreen) {
+    docElm.webkitRequestFullScreen();
+  } else if (docElm.mozRequestFullScreen) {
+    docElm.mozRequestFullScreen();
+  } else if (docElm.msRequestFullscreen) {
+    docElm.msRequestFullscreen();
+  }
 });
 
-// reset button & action
-let resetBtn = document.getElementById("reset-btn");
-let scoreDisplay = document.getElementById("scoreDisplay");
-let progressBar = document.getElementById("progress-bar");
-
-let score = 0;
-
-resetBtn.addEventListener("click", function () {
- score = 0;
- scoreDisplay.textContent = score;
- progressBar.style.width = "0%";
+// Restart button
+let restartBtn = document.getElementById("reset-btn");
+restartBtn.addEventListener("click", function () {
+  currentImageIndex = 0;
+  currentImage.src = imageArray[currentImageIndex];
+  input.value = "";
+  currentIndex = 0;
+  document.getElementById("correct-msg").textContent = "";
+  let reservoir = document.getElementById("reservoir");
+  reservoir.style.background = "transparent";
+  correctGuesses = 0;
 });
-
 
 //IMAGE DISPLAYED
 let imageArray = [
- 'Image_reference/earth.png',
- 'Image_reference/saturn.png',
- 'Image_reference/mars.png',
- 'Image_reference/uranus.png'
+  "Image_reference/earth.png",
+  "Image_reference/saturn.png",
+  "Image_reference/mars.png",
+  "Image_reference/uranus.png",
 ];
 
 let currentImageIndex = 0;
-let currentImage = document.createElement('img');
+let currentImage = document.createElement("img");
 currentImage.src = imageArray[currentImageIndex];
-currentImage.style.width = '22rem';
-currentImage.style.height = '20rem';
-document.getElementById('image-array').appendChild(currentImage);
+currentImage.style.width = "22rem";
+currentImage.style.height = "20rem";
+document.getElementById("image-array").appendChild(currentImage);
 
-let keys = document.querySelectorAll('.key');
-let input = document.getElementById('input-bar');
-let planetWords = ['EARTH', 'SATURN', 'MARS', 'URANUS'];
+//words typed
+let keys = document.querySelectorAll(".key");
+let input = document.getElementById("input-bar");
+let planetWords = ["EARTH", "SATURN", "MARS", "URANUS"];
 let currentIndex = 0;
+let correctGuesses = 0;
 
-keys.forEach(function(key) {
- key.addEventListener('click', function() {
-   let letter = key.textContent;
-   let word = planetWords[currentImageIndex];
+keys.forEach(function (key) {
+  key.addEventListener("click", function () {
+    let letter = key.textContent;
+    let word = planetWords[currentImageIndex];
 
-   if (letter === word[currentIndex]) {
-     input.value += letter;
-     currentIndex++;
+    if (letter === word[currentIndex]) {
+      input.value += letter;
+      currentIndex++;
 
-     if (currentIndex === word.length) {
-       input.value = word;
-       currentIndex = 0;
-       setTimeout(function() {
-         document.getElementById('correct-msg').textContent = 'CORRECT!';
-         setTimeout(function() {
-           currentImageIndex = (currentImageIndex + 1) % imageArray.length;
-           currentImage.src = imageArray[currentImageIndex];
-           document.getElementById('correct-msg').textContent = '';
-           input.value = ''; 
-         }, 2000);
-       }, 0);
-     }
-   } else {
-     input.value = '';
-     currentIndex = 0;
-   }
- });
+      if (currentIndex === word.length) {
+        input.value = word;
+        currentIndex = 0;
+        setTimeout(function () {
+          document.getElementById("correct-msg").textContent = "CORRECT!";
+
+          // Filling the reservoir
+          let reservoir = document.getElementById("reservoir");
+          correctGuesses++;
+          let heightToFill = reservoir.offsetHeight * (0.25 * correctGuesses);
+          let filledHeight = 0;
+          let intervalId = setInterval(function () {
+            if (filledHeight >= heightToFill) {
+              clearInterval(intervalId);
+            } else {
+              filledHeight += 5;
+              reservoir.style.background = `linear-gradient(to top, #f5c156 ${filledHeight}px, transparent ${filledHeight}px)`;
+            }
+          }, 50);
+
+          setTimeout(function () {
+            currentImageIndex = (currentImageIndex + 1) % imageArray.length;
+            currentImage.src = imageArray[currentImageIndex];
+            document.getElementById("correct-msg").textContent = "";
+            input.value = "";
+
+            // Reseting the reservoir bar
+            if (correctGuesses === planetWords.length) {
+              setTimeout(function () {
+                reservoir.style.background = "transparent";
+                correctGuesses = 0;
+              }, 500);
+            }
+          }, 2700);
+        }, 0);
+      }
+    } 
+  });
 });
 
-
-
-//play button
+//sounds bites
 let soundArray = [
- 'Sound-bites/earth.wav',
- 'Sound-bites/saturn.wav',
- 'Sound-bites/mars.wav',
- 'Sound-bites/uranus.wav'
+  "Sound-bites/earth.wav",
+  "Sound-bites/saturn.wav",
+  "Sound-bites/mars.wav",
+  "Sound-bites/uranus.wav",
 ];
 
 const playBtn = document.getElementById("play-btn");
 const mySound = document.getElementById("my-Sound");
 
 playBtn.addEventListener("click", function () {
- if (mySound.paused) {
-   mySound.src = soundArray[currentImageIndex];
-   mySound.play();
- } else {
-   mySound.pause();
- }
+  if (mySound.paused) {
+    mySound.src = soundArray[currentImageIndex];
+    mySound.play();
+  } else {
+    mySound.pause();
+  }
 });
-
